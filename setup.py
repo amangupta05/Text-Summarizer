22c3176f2578fe8464fa0852219c6dbab15a788c
amangupta05 <amangupta52001@gmail.com>
2023-06-03 19:27:04 +0530

    deploy

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
 import setuptools
+
 with open("README.md", "r", encoding="utf-8") as f:
-    long_description= f.read()
-
-__version__="0.0.0"
+    long_description = f.read()
 
 
-REPO_NAME = "Text-Summarizert"
+__version__ = "0.0.0"
+
+REPO_NAME = "Text-Summarizer-Project"
 AUTHOR_USER_NAME = "amangupta05"
-SRC_REPO = "textSummarizer"
+SRC_REPO = "Text-Summarizer"
 AUTHOR_EMAIL = "amangupta52001@gmail.com"
 
 
